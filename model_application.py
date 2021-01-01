bd723d048245319e4a13da6ca3458cd3a1ed1be4
Estela23 <estela.monxardina@gmail.com>
2021-01-01 19:58:53 +0100

    modified LGBM with feature selection techniques and added correspondant results

--- model_application.py
+++ model_application.py
@@ -2,76 +2,45 @@ from lightgbm import LGBMRegressor
 from sklearn import model_selection, metrics
 from sklearn.model_selection import KFold
 import evaluation
-# import tensorflow as tf
 import pandas as pd
 import os
 import numpy as np
-from aplying_nn import load_test_data
-
-
-def apply_LGBM_regression(train_data, y_train, validation_data, y_val):
-    lgb = LGBMRegressor(random_state=100, n_estimators=2000)  # ,max_depth=7,n_estimators=250,learning_rate=0.12
-    lgb.fit(train_data, y_train)
-    prediction = lgb.predict(validation_data)
-    return prediction
-
-
-# path_local = 'C:/Users/Tair/Documents/MAI Semester1/CI/Project'
-file_name = 'train_final_data_complete.csv'
-df_data = pd.read_csv(file_name)
-#  feature selection
-df_reduced_data = df_data.drop(columns=["segment_id"])  # for debug only!!!!!!!!!!!!!!!!!!!!
-train, val, y, y_val = model_selection.train_test_split(df_reduced_data[df_reduced_data.columns[:-1]],
-                                                        df_reduced_data[df_reduced_data.columns[-1]],
-                                                        test_size=0.2, shuffle=True)
-
-# apply LGBM
-preds = apply_LGBM_regression(train, y, val, y_val)
-mse, rmse, mae = evaluation.all_errors(y_val, preds)
-
-
-# apply NN
-# preds_nn = apply_NN(train, y, val)
-# evaluation.rMSE(y_val, preds_nn)
+from FeatureSelection import FeatureSelectionUsingCorrelation, FeatureSelectionWrapper, FeatureSelectionEmbedded
 
 
 def apply_kfold_LGBM(number_of_folds, train, y, validation_set, y_val):
     kfold = KFold(n_splits=number_of_folds, random_state=1, shuffle=True).split(y)
-    predictions = []
     models = []
-
     for i, (train_i, test_i) in enumerate(kfold):
         print(f'Fold {i}')
-        model = LGBMRegressor(random_state=100, n_estimators=2000)  # ,max_depth=7,n_estimators=250,learning_rate=0.12
+        model = LGBMRegressor(random_state=100)
         model.fit(train.values[train_i], y.values[train_i])
         pred = model.predict(validation_set)
         evaluation.all_errors(y_val, pred)
-        predictions.append(pred)
-        # print('Fold rmse', rmse(y.values[test_i], model.predict(train.values[test_i])))
         models.append(model)
-
-    # res_predictions = predictions[0]
-    # for i in range(1, number_of_folds):
-    #     res_predictions += predictions[i]
-    # res_predictions /= number_of_folds
-    #
-    # print('LGBM rmse', rmse(y_val, res_predictions))
-
     return models
 
 
-def test_LGBM(train_data, validation_data, y_train, y_validation, folds):
-    test_set = load_test_data("test_final_data_complete.csv")
+def test_LGBM(train_data, validation_data, y_train, y_validation, folds, output_file_name, reduced_technique):
+    test_set = pd.read_csv("test_final_data_complete.csv")
+    if reduced_technique == 1:
+        test_set_reduced = test_set[list_features_corr[:-1]]
+        test_set_reduced["segment_id"] = test_set["segment_id"]
+    elif reduced_technique == 2:
+        test_set_reduced = test_set[list_features_wrapper]
+        test_set_reduced["segment_id"] = test_set["segment_id"]
+    else:
+        test_set_reduced = test_set
 
     models = apply_kfold_LGBM(folds, train_data, y_train, validation_data, y_validation)  # create models using k-fold
 
     # predict in test set with all models
     predictions = []
     for model in models:
-        pred = model.predict(test_set.drop(['segment_id'], axis=1))
+        pred = model.predict(test_set_reduced.drop("segment_id", 1))
         predictions.append(pred)
 
-    # do the average of the multiple predictions
+    # average of the multiple predictions
     res_preds = predictions[0]
     for i in range(1, folds):
         res_preds += predictions[i]
@@ -81,11 +50,39 @@ def test_LGBM(train_data, validation_data, y_train, y_validation, folds):
     sample_submission = pd.read_csv('sample_submission.csv')
 
     test_set['time_to_eruption'] = res_preds
-    sample_submission = pd.merge(sample_submission.drop(['time_to_eruption_x'], axis=1), test_set[['segment_id', 'time_to_eruption']], on='segment_id')
+    sample_submission = pd.merge(sample_submission, test_set[['segment_id', 'time_to_eruption']], on='segment_id')
     sample_submission = sample_submission.drop(['time_to_eruption_x'], axis=1)
     sample_submission.columns = ['segment_id', 'time_to_eruption']
 
-    sample_submission.to_csv('results/first_submission_LGBM.csv', index=False)
+    sample_submission.to_csv(f'results/{output_file_name}.csv', index=False)
 
 
-test_LGBM(train, val, y, y_val, folds=10)
+df_data = pd.read_csv("train_final_data_complete.csv")
+df_data = df_data.drop(columns=["segment_id"])
+
+# Original Data split
+train, val, y, y_val = model_selection.train_test_split(df_data[df_data.columns[:-1]],
+                                                        df_data[df_data.columns[-1]],
+                                                        test_size=0.2, shuffle=True)
+
+# Correlation Reduced Data split
+list_features_corr, df_reduced_corr_data = FeatureSelectionUsingCorrelation(df_data, 0.05)
+
+corr_train, corr_val, corr_y, corr_y_val = model_selection.train_test_split(df_reduced_corr_data[df_reduced_corr_data.columns[:-1]],
+                                                                            df_reduced_corr_data[df_reduced_corr_data.columns[-1]],
+                                                                            test_size=0.2, shuffle=True)
+
+# Wrapper Reduced Data split
+list_features_wrapper, df_reduced_wrapper_data = FeatureSelectionWrapper(df_data, 0.5)
+
+wrapper_train, wrapper_val, wrapper_y, wrapper_y_val = model_selection.train_test_split(df_reduced_wrapper_data[df_reduced_wrapper_data.columns[:-1]],
+                                                                                        df_reduced_wrapper_data[df_reduced_wrapper_data.columns[-1]],
+                                                                                        test_size=0.2, shuffle=True)
+
+# Final predictions
+print("LGBM with original data")
+test_LGBM(train, val, y, y_val, folds=10, output_file_name="original_data_submission_LGBM", reduced_technique=0)
+print("LGBM with correlation reduced data")
+test_LGBM(corr_train, corr_val, corr_y, corr_y_val, folds=10, output_file_name="correlation_reduced_data_submission_LGBM", reduced_technique=1)
+print("LGBM with wrapper reduced data")
+test_LGBM(wrapper_train, wrapper_val, wrapper_y, wrapper_y_val, folds=10, output_file_name="wrapper_reduced_data_submission_LGBM", reduced_technique=2)
